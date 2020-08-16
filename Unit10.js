e776585273e9d0fcc0641e799b63741ba263f7d6
strofimov7 <sergei.v.trofimov@gmail.com>
2020-08-16 18:01:39 +0200

    tasks done for unit 10

--- Unit10.js
+++ Unit10.js
@@ -31,7 +31,6 @@ function taskThreeFour(s) {
     let arr = s.split("");
     let reverseArr = arr.reverse();
     let reverseS = reverseArr.join();
-
     console.log(reverseS);
 
     let m = Math.round(Math.random() * 100);
@@ -39,5 +38,53 @@ function taskThreeFour(s) {
 
 }
 
-function taskFive() {}
+function taskFive(arr) {
+    console.log(arr.length);
+    for(let i = 0; i<arr.length; i++) {
+        console.log(arr[i]);
+    }
+
+}
+
+function taskSix(arr) {
+    let flag = true;
+    for(let i = 0; i<arr.length-1; i++) {
+
+            if (arr[i] != arr[i+1])
+                flag = false;
+    }
+    console.log(flag);
+}
+
+function taskSeven(arr) {
+    let odd = 0;
+    let even = 0;
+    let zero = 0;
+    for(i = 0; i<arr.length; i++) {
+        if ((typeof(arr[i]) === "number") && (Math.round(arr[i],0) != 0) && (! arr[i].isNaN) & (arr[i] !== undefined)) {
+            odd = odd + (Math.round(arr[i],0) % 2);
+            even = even + (Math.round(arr[i],0) % 2);
+        }
+        else if (arr[i] === 0) {
+            zero++;
+        }
+
+
+    }
+    console.log(odd);
+    console.log(even);
+    console.log(zero);
+}
+
+function taskEight() {
+    let mp = new Map();
+    mp.set("user", "tcv");
+    mp.set("98", "pwdx");
+    mp.set("cat", "Mars");
+    mp.set(false, "key_bool");
+    for (let elem of mp.keys()) {
+        console.log("key : " + elem + "; value : " + mp.get(elem));
+    }
+
+}
 
