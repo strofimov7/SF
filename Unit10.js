814459f5f0ab9b7183becc78924dede2446af1bd
strofimov7 <sergei.v.trofimov@gmail.com>
2020-08-16 11:11:54 +0200

    commit through WS

---
+++ Unit10.js
@@ -0,0 +1,45 @@
+function taskOne() {
+    let a = prompt("Enter Number");
+    let b = +a;
+    if (typeof b == "number" && b != NaN)
+        console.log(b % 2);
+    else
+        console.log("error");
+
+}
+
+function taskTwo(X) {
+    let typeVar = typeof X;
+
+    switch (typeVar) {
+        case "number":
+            console.log("X - number");
+            break;
+        case "string":
+            console.log("X - string");
+            break;
+        case "boolean":
+            console.log("X - boolean")
+            break;
+        default:
+            console.log("X - undefined");
+
+    }
+}
+
+function taskThreeFour(s) {
+    let arr = s.split("");
+    let reverseArr = arr.reverse();
+    var reverseS = reverseArr.join();
+
+    console.log(reverseS);
+
+    let m = Math.round(Math.random() * 100);
+    console.log(m);
+
+}
+
+function taskFive() {
+
+
+}
